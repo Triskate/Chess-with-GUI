a03437bcd115b67364e5a7938743c6e2294c1115
Albert Isla <albertislatorres@gmail.com>
2024-05-23 13:28:31 +0200

    Added Bishop and Queen behaviour+ Optimization
    Both new added behaviours need the going through pieces fix

--- src/main/java/chess/Rules.java
+++ src/main/java/chess/Rules.java
@@ -7,154 +7,135 @@ public class Rules {
         String currentPiece = Board.main_board[currentRow][currentColumn];
         String aimedPiece = Board.main_board[aimedRow][aimedColumn];
 
+        // Pawn movements
+        boolean shortForward = aimedRow == currentRow-1 && aimedPiece.equals("  ");
+        boolean diagonalKill = (aimedRow == currentRow-1) && (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1);
+
+        // Horse movements
+        boolean L1 = aimedRow == currentRow+2 && (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1);
+        boolean L2 = aimedRow == currentRow-2 && (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1);
+        boolean L3 = aimedRow == currentRow+1 && (aimedColumn == currentColumn+2 || aimedColumn == currentColumn-2);
+        boolean L4 = aimedRow == currentRow-1 && (aimedColumn == currentColumn+2 || aimedColumn == currentColumn-2);
+
+        // Bishop movement
+        boolean diagonal = Math.abs(currentRow - aimedRow)  == Math.abs(currentColumn - aimedColumn);
+
+        // Tower movements
+        boolean up = aimedRow == currentRow && aimedColumn < currentColumn;
+        boolean down = aimedRow == currentRow && aimedColumn > currentColumn;
+        boolean left = aimedRow < currentRow && aimedColumn == currentColumn;
+        boolean right = aimedRow > currentRow && aimedColumn == currentColumn;
+
+        // King movement
+        boolean shortStep = (aimedRow == currentRow-1 || aimedRow == currentRow+1 || aimedRow == currentRow) &&
+                            (aimedColumn == currentColumn-1 || aimedColumn == currentColumn+1 || aimedColumn == currentColumn);
+
+        // Queen movements
+        boolean vertical = (aimedRow == currentRow && aimedColumn != currentColumn);
+        boolean horizontal = (aimedRow != currentRow && aimedColumn == currentColumn);;
+
         switch(currentPiece){
             // Validation for white pieces
 
             // Pawn
             case "WP":
-                if(aimedRow == currentRow-1 && aimedPiece.equals("  ")){
-                    return true;
-                }else if(aimedPiece.startsWith("B") &&
-                        (aimedRow == currentRow-1) &&
-                        (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1)){
-                    return true;
-                }else{
-                    return false;
-                }
+                return shortForward || (aimedPiece.startsWith("B") && diagonalKill);
             // Horse
             case "WH":
-                if((aimedPiece.startsWith("B") || aimedPiece.equals("  ")) && (
-                   (aimedRow == currentRow+2 && (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1)) ||
-                   (aimedRow == currentRow-2 && (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1)) ||
-                   (aimedRow == currentRow+1 && (aimedColumn == currentColumn+2 || aimedColumn == currentColumn-2)) ||
-                   (aimedRow == currentRow-1 && (aimedColumn == currentColumn+2 || aimedColumn == currentColumn-2)))){
-                    return true;
-                }
-                else{
-                    return false;
-                }
+                return (aimedPiece.startsWith("B") || aimedPiece.equals("  ")) && (L1 || L2 || L3 || L4);
+            // Bishop (Need to fix jumping through pieces)
             case "WB":
-                break;
+                return (aimedPiece.startsWith("B") || aimedPiece.equals("  ")) && diagonal;
             // Tower
             case "WT":
-                if(aimedPiece.equals("  ") || aimedPiece.startsWith("B")) {
-                    if (aimedRow > currentRow && aimedColumn == currentColumn) {
+                if(aimedPiece.startsWith("B") || aimedPiece.equals("  ")) {
+                    if (right) {
                         for (int i = currentRow; i < aimedRow; i++) {
                             if (!Board.main_board[i][currentColumn].equals("  ")) {
                                 return false;
                             }
                         }
                         return true;
-                    } else if (aimedRow < currentRow && aimedColumn == currentColumn) {
+                    } else if (left) {
                         for (int i = aimedRow; i < currentRow; i++) {
                             if (!Board.main_board[i][currentColumn].equals("  ")) {
                                 return false;
                             }
                         }
                         return true;
-                    } else if (aimedRow == currentRow && aimedColumn > currentColumn) {
+                    } else if (down) {
                         for (int i = currentColumn; i < aimedColumn; i++) {
                             if (!Board.main_board[currentRow][i].equals("  ")) {
                                 return false;
                             }
                         }
                         return true;
-                    } else if (aimedRow == currentRow && aimedColumn < currentColumn) {
+                    } else if (up) {
                         for (int i = aimedColumn; i < currentColumn; i++) {
                             if (!Board.main_board[currentRow][i].equals("  ")) {
                                 return false;
                             }
                         }
                         return true;
-                    } else {
-                        return false;
                     }
                 }
+                return false;
             // King
             case "WK":
-                if((aimedPiece.equals("  ") || aimedPiece.startsWith("B")) && (
-                  (aimedRow == currentRow-1 || aimedRow == currentRow+1 || aimedRow == currentRow) &&
-                  (aimedColumn == currentColumn-1 || aimedColumn == currentColumn+1 || aimedColumn == currentColumn))){
-                    return true;
-                } else {
-                    return false;
-                }
+                return (aimedPiece.startsWith("B") || aimedPiece.equals("  ")) && shortStep;
             case "WQ":
-                break;
+                return (aimedPiece.startsWith("B") || aimedPiece.equals("  ")) && (horizontal || vertical || diagonal);
 
             // Validation for black pieces
 
             // Pawn
             case "BP":
-                if(aimedRow == currentRow+1 && aimedPiece.equals("  ")){
-                    return true;
-                }else if(aimedPiece.startsWith("W") &&
-                        (aimedRow == currentRow+1) &&
-                        (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1)){
-                    return true;
-                }else{
-                    return false;
-                }
+                return shortForward || (aimedPiece.startsWith("W") && diagonalKill);
             // Horse
             case "BH":
-                if((aimedPiece.startsWith("W") || aimedPiece.equals("  ")) && (
-                   (aimedRow == currentRow+2 && (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1)) ||
-                   (aimedRow == currentRow-2 && (aimedColumn == currentColumn+1 || aimedColumn == currentColumn-1)) ||
-                   (aimedRow == currentRow+1 && (aimedColumn == currentColumn+2 || aimedColumn == currentColumn-2)) ||
-                   (aimedRow == currentRow-1 && (aimedColumn == currentColumn+2 || aimedColumn == currentColumn-2)))){
-                    return true;
-                }
-                else{
-                    return false;
-                }
+                return (aimedPiece.startsWith("W") || aimedPiece.equals("  ")) && (L1 || L2 || L3 || L4);
+            // Bishop (Need to fix jumping through pieces)
             case "BB":
-                break;
+                return (aimedPiece.startsWith("W") || aimedPiece.equals("  ")) && diagonal;
             // Tower
             case "BT":
-                if(aimedPiece.equals("  ") || aimedPiece.startsWith("W")) {
-                    if (aimedRow > currentRow && aimedColumn == currentColumn) {
+                if(aimedPiece.startsWith("W") || aimedPiece.equals("  ")){
+                    if (right) {
                         for (int i = currentRow; i < aimedRow; i++) {
                             if (!Board.main_board[i][currentColumn].equals("  ")) {
                                 return false;
                             }
                         }
                         return true;
-                    } else if (aimedRow < currentRow && aimedColumn == currentColumn) {
+                    } else if (left) {
                         for (int i = aimedRow; i < currentRow; i++) {
                             if (!Board.main_board[i][currentColumn].equals("  ")) {
                                 return false;
                             }
                         }
                         return true;
-                    } else if (aimedRow == currentRow && aimedColumn > currentColumn) {
+                    } else if (down) {
                         for (int i = currentColumn; i < aimedColumn; i++) {
                             if (!Board.main_board[currentRow][i].equals("  ")) {
                                 return false;
                             }
                         }
                         return true;
-                    } else if (aimedRow == currentRow && aimedColumn < currentColumn) {
+                    } else if (up) {
                         for (int i = aimedColumn; i < currentColumn; i++) {
                             if (!Board.main_board[currentRow][i].equals("  ")) {
                                 return false;
                             }
                         }
                         return true;
-                    } else {
-                        return false;
                     }
                 }
+                return false;
             // King
             case "BK":
-                if(aimedPiece.equals("  ") || aimedPiece.startsWith("W") && (
-                  (aimedRow == currentRow-1 || aimedRow == currentRow+1 || aimedRow == currentRow) &&
-                  (aimedColumn == currentColumn-1 || aimedColumn == currentColumn+1 || aimedColumn == currentColumn))){
-                    return true;
-                } else {
-                    return false;
-                }
+                return (aimedPiece.startsWith("W") || aimedPiece.equals("  ")) && shortStep;
             case "BQ":
-                break;
+                return (aimedPiece.startsWith("W") || aimedPiece.equals("  ")) && (horizontal || vertical || diagonal);
         }
         return false;
     }
