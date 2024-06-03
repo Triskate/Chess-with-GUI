a1ff5aad42f85ce77ec15c588b9a08e59649ae7e
Albert Isla <albertislatorres@gmail.com>
2024-06-03 11:54:48 +0200

    Added commentaries and optimized the code

--- src/main/java/chess/Promotion_GUI.java
+++ src/main/java/chess/Promotion_GUI.java
@@ -7,8 +7,7 @@ import java.util.Objects;
 
 public class Promotion_GUI extends JPanel {
 
-
-    public String startPromotionGUI(String Color, int aimedRow, int aimedColumn){
+    public void startPromotionGUI(String Color, int aimedRow, int aimedColumn){
 
         // Declare GUI
         JFrame frame = new JFrame("Promotion");
@@ -17,52 +16,55 @@ public class Promotion_GUI extends JPanel {
         frame.setSize(270,150);
         frame.setLayout(null);
 
+        // Promotion text
         JLabel promotionText = new JLabel();
         promotionText.setText("Promotion!");
         promotionText.setBounds(100,10,200,40);
         frame.getContentPane().add(promotionText);
 
+        // Pieces buttons
         JButton horse = new JButton();
         JButton tower = new JButton();
         JButton bishop = new JButton();
         JButton queen = new JButton();
 
+        // Declare values of the buttons
         horse.setBounds(20,50,50,50);
         tower.setBounds(75,50,50,50);
         bishop.setBounds(130,50,50,50);
         queen.setBounds(185,50,50,50);
-
         switch(Color){
             case "W":
+                // White horse
                 horse.setToolTipText("WH");
                 horse.setIcon(GUI.upscaleIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("/icons/WH.png"))))));
-
+                // White tower
                 tower.setToolTipText("WT");
                 tower.setIcon(GUI.upscaleIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("/icons/WT.png"))))));
-
+                // White bishop
                 bishop.setToolTipText("WB");
                 bishop.setIcon(GUI.upscaleIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("/icons/WB.png"))))));
-
+                // White queen
                 queen.setToolTipText("WQ");
                 queen.setIcon(GUI.upscaleIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("/icons/WQ.png"))))));
                 break;
             case "B":
+                // Black horse
                 horse.setToolTipText("BH");
                 horse.setIcon(GUI.upscaleIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("/icons/BH.png"))))));
-
+                // Black tower
                 tower.setToolTipText("BT");
                 tower.setIcon(GUI.upscaleIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("/icons/BT.png"))))));
-
+                // Black bishop
                 bishop.setToolTipText("BB");
                 bishop.setIcon(GUI.upscaleIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("/icons/BB.png"))))));
-
+                // Black queen
                 queen.setToolTipText("BQ");
                 queen.setIcon(GUI.upscaleIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("/icons/BQ.png"))))));
                 break;
             default:
                 break;
         }
-
         horse.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 executePromotion(horse.getToolTipText(), aimedRow, aimedColumn);
@@ -88,14 +90,14 @@ public class Promotion_GUI extends JPanel {
             }
         });
 
+        // Add buttons to the frame
         frame.getContentPane().add(horse);
         frame.getContentPane().add(tower);
         frame.getContentPane().add(bishop);
         frame.getContentPane().add(queen);
 
+        // Set frame visible
         frame.setVisible(true);
-
-        return null;
     }
 
     public void executePromotion(String piece, int currentRow, int currentColumn){
